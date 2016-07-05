a38882e7a8408775e0234c1292623fa9a498ec17
Edgar F. Fuentes Perea (EFP) <efuentes@softtek.com>
2016-07-05 16:29:42 -0500

    Routes added to app.component

--- app/app.component.ts
+++ app/app.component.ts
@@ -9,21 +9,31 @@ import { HomeComponent } from './home/home.component';
 
 import { AddFideicomisoComponent } from './fideicomiso/add-fideicomiso.component';
 import { EditFideicomisoComponent } from './fideicomiso/edit-fideicomiso.component';
+import { FideicomisoService } from './fideicomiso/fideicomiso.service';
+
+import { AddSubfisoComponent } from './subfiso/add-subfiso.component';
+import { EditSubfisoComponent } from './subfiso/edit-subfiso.component';
+import { SubfisoService } from './subfiso/subfiso.service';
 
 import { FideicomisosComponent } from './fideicomisos/fideicomisos.component';
-
-import { FideicomisoService } from './fideicomiso/fideicomiso.service';
+import { SubfisosComponent } from './subfisos/subfisos.component';
 
 @Component({
 	selector: 'app',
 	templateUrl: 'app/app.component.html',
 	directives: [ROUTER_DIRECTIVES, TopNavbarComponent, HomeComponent],
-	providers: [HTTP_PROVIDERS, ROUTER_PROVIDERS, FideicomisoService]
+	providers: [HTTP_PROVIDERS, ROUTER_PROVIDERS,
+				FideicomisoService, SubfisoService]
 })
 @RouteConfig([
 	{ path: '/home', name: 'Home', component: HomeComponent, useAsDefault: true },
+	
 	{ path: '/fideicomiso/add', name: 'AddFideicomiso', component: AddFideicomisoComponent },
 	{ path: '/fideicomiso/edit', name: 'EditFideicomiso', component: EditFideicomisoComponent },
-	{ path: '/fideicomisos', name: 'Fideicomisos', component: FideicomisosComponent }
+	{ path: '/fideicomisos', name: 'Fideicomisos', component: FideicomisosComponent },
+	
+	{ path: '/subfiso/add', name: 'AddSubfiso', component: AddSubfisoComponent },
+	{ path: '/subfiso/edit', name: 'EditSubfiso', component: EditSubfisoComponent },
+	{ path: '/subfisos', name: 'Subfisos', component: SubfisosComponent }
 ])
 export class AppComponent { }	
